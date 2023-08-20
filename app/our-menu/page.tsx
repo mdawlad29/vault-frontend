28dd909f1eb6186f9427368971fc3bbfa31ffc9a
mdawlad29 <mdawladbdinfo@gmail.com>
2023-08-21 00:38:39 +0600

    About & page added

--- app/our-menu/page.tsx
+++ app/our-menu/page.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+"use client";
 import dynamic from "next/dynamic";
+import { Box, Typography } from "@mui/material";
 const GlobalMetadata = dynamic(() => import("../globalMetadata"), {
   ssr: false,
 });
@@ -9,6 +10,16 @@ const OurMenuPage = () => {
     <>
       <GlobalMetadata title="Our menu - Foody" description="Foody portfolio" />
       OurMenuPage
+      <Box className="w-full h-screen flex justify-center items-center dark:bg-primary dark:text-neutral">
+        <Typography
+          component={"span"}
+          variant="h4"
+          display={"block"}
+          textTransform={"uppercase"}
+        >
+          coming soon..!
+        </Typography>
+      </Box>
     </>
   );
 };
